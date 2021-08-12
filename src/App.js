4e3ce62f1016c2a144d42c5bc59d74c52ff651f5
DaimSyed <daimshah784@gmail.com>
2021-08-12 06:35:25 +0500

    Lazy Loaing Added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import NewQoute from "./Pages/NewQoute";
-import AllQoutes from "./Pages/AllQoutes";
-import QouteDetail from "./Pages/QouteDetail";
-import Layout from "./components/layout/Layout";
-import NotFound from "./Pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+const NotFound = React.lazy(() => import("./Pages/NotFound"));
+const Layout = React.lazy(() => import("./components/layout/Layout"));
+const QouteDetail = React.lazy(() => import("./Pages/QouteDetail"));
+const AllQoutes = React.lazy(() => import("./Pages/AllQoutes"));
+const NewQoute = React.lazy(() => import("./Pages/NewQoute"));
 const App = () => {
   return (
-    <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/qoutes" />
-        </Route>
-        <Route path="/qoutes" exact>
-          <AllQoutes />
-        </Route>
-        <Route path="/new-qoute">
-          <NewQoute />
-        </Route>
-        <Route path="/qoutes/:qouteId">
-          {" "}
-          {/* Component = {component} it will give history object to props */}
-          <QouteDetail /> {/* it will not give history object to props */}
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-    </Layout>
+    <Suspense
+      fallback={
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      }
+    >
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/qoutes" />
+          </Route>
+          <Route path="/qoutes" exact>
+            <AllQoutes />
+          </Route>
+          <Route path="/new-qoute">
+            <NewQoute />
+          </Route>
+          <Route path="/qoutes/:qouteId">
+            {" "}
+            {/* Component = {component} it will give history object to props */}
+            <QouteDetail /> {/* it will not give history object to props */}
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Layout>
+    </Suspense>
   );
 };
 
